db8942a3684bf7bff0bbd17e6aa5371eac743033
seyiclover <leeseyi31@gmail.com>
2026-09-17 14:34:02 +0000

    sync: rebuild seyiclover programmers solutions

--- seyiclover/1주차/2/42626. 더 맵게/더 맵게.py
+++ seyiclover/1주차/2/42626. 더 맵게/더 맵게.py
@@ -1,19 +1,28 @@
 import heapq
 
 def solution(scoville, K):
-    heapq.heapify(scoville)
-    count = 0
+    cnt = 0
 
+    # 리스트를 최소 힙으로 변환
+    heapq.heapify(scoville)
+
+    # 음식이 2개 이상이고, 최솟값이 K보다 작은 동안 반복
     while len(scoville) >= 2 and scoville[0] < K:
+
+        # 가장 작은 두 음식 꺼내기
         first = heapq.heappop(scoville)
         second = heapq.heappop(scoville)
 
+        # 새로운 스코빌 지수 계산
         new = first + second * 2
+
+        # 다시 힙에 넣기
         heapq.heappush(scoville, new)
 
-        count += 1
+        cnt += 1
 
+    # 더 이상 섞을 수 없는데도 K보다 작다면 실패
     if scoville[0] < K:
         return -1
 
-    return count
+    return cnt
