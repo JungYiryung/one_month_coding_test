f0852e5e9dc3e5da77e47311b819a398a1163124
seyiclover <leeseyi31@gmail.com>
2026-09-17 12:15:22 +0000

    sync: rebuild seyiclover programmers solutions

--- seyiclover/1주차/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ seyiclover/1주차/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -1,3 +1,4 @@
+# 원본 배열에서 현재 값과 다음 값을 비교하는 방식
 def solution(arr):
     answer = []
     for i in range(len(arr)-1):
@@ -6,3 +7,13 @@ def solution(arr):
     answer.append(arr[-1])
     
     return answer
+
+# 스택을 활용한 풀이
+def solution(arr):
+    result = []
+    
+    for num in arr:
+        if len(result) == 0 or result[-1] != num:
+            result.append(num)
+        
+    return result
